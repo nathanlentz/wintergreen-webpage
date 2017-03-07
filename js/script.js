fdc2978c9dbaecf5cd81afcc403cd86cc5fce205
Nate Lentz <nathanlentz@outlook.com>
2017-03-06 23:58:54 -0500

    Adding dev panel and content to main page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
+// Document Ready
+$(function(){
+    $('.slide-tab-lead').hover(function(){
+        $(this).stop(true, false).animate({ left: 0 }, 400);
+        $('.slide-tab-back').stop(true,false).animate({ left: 0 }, 400);
+
+    }, function(){
+        $(this).stop(true, false).animate({ left: -120 }, 400);
+        $('.slide-tab-back').stop(true,false).animate({ left: -190 }, 400);
+    });
+});
+
+
 $('.menu-toggle').click(function() {
     $(".nav-bar").toggleClass("nav-bar--open", 500);
     $(this).toggleClass('ham-open');
 })
+
